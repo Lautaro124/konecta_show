f94eb6d538e1aed27016a4f0052c35e74e12192f
Lautaro Gonzalez <59624470+Lautaro124@users.noreply.github.com>
2024-03-11 14:59:46 -0300

    UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,18 @@ export default async function Home() {
   const todos = await service()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen p-20 grid grid-cols-3 gap-2">
       {
         todos.map(todo => (
-          <section key={todo.id}>
-            <h3>{todo.title}</h3>
-            <span>User ID: {todo.userId}</span>
+          <section key={todo.id} className='w-full h-36 p-4 rounded-md flex flex-col justify-between gap-3 shadow-md shadow-slate-800'>
+            <div>
+              <h2 className='underline underline-offset-4 italic'>Title</h2>
+              <h3 className='font-semibold text-lg truncate '>{todo.title}</h3>
+            </div>
+            <div className='grid grid-rows-2'>
+              <span className='italic'>User ID: {todo.userId}</span>
+              <span className='italic'>Status: {todo.completed ? 'Completed' : 'Incompleted'}</span>
+            </div>
           </section>
         ))
       }
